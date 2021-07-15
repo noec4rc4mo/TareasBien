08be40f32a8ab8f8ac6bf6d17888bb33aeca1184
noec4rc4mo <noec4rc4mo@gmail.com>
2021-07-14 21:39:51 -0600

    Primer commit de la tarea módulo 1 - Ejercicio 5

---
+++ src/main/java/com/mycompany/mavenproject2/Ejercicio5.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author noec
+ */
+public class Ejercicio5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("Números pares del 2 al 100");
+        for (int i = 2; i <= 100; i = i + 2) {
+            System.out.println(i);
+    }
+    }
+    
+}
