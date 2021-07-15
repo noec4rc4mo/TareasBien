951c2ff9ee7142e88b70d75104451c25e67f7270
noec4rc4mo <noec4rc4mo@gmail.com>
2021-07-14 21:38:53 -0600

    Primer commit de la tarea módulo 1 - Ejercicio 3

---
+++ src/main/java/com/mycompany/mavenproject2/Ejercicio3.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author noec
+ */
+public class Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+        //Declaración de variables
+        int M=6;
+        int T=1;
+        int K=-10;
+        
+        //Evaluación
+        if(M>K){
+        System.out.println("1. M>K es Verdadero");
+        }
+        else {
+        System.out.println("1. M>K es Falso");
+        }
+        
+        if(T/K==5){
+        System.out.println("2. T/K es Verdadero");
+        }
+        else{
+        System.out.println("2. L/K es Falso");
+        }
+        
+        if(M+T==7 && M-T==5){
+        System.out.println("3. M+T=7 || M-T=5 es Verdadero");
+        }
+        else{
+        System.out.println("3. M+T=7 || M-T=5 es Falso");
+        }
+    
+        }
+    }
+   
