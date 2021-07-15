deb9966d2e8bdd6413a1b5e2b2329b6a6c5e2ac3
noec4rc4mo <noec4rc4mo@gmail.com>
2021-07-14 21:39:23 -0600

    Primer commit de la tarea módulo 1 - Ejercicio 4

---
+++ src/main/java/com/mycompany/mavenproject2/Ejercicio4.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author noec
+ */
+public class Ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    String[] grupo = new String[20];
+    grupo[0] = "Lima";
+    grupo[1] = "Guadalajara";
+    grupo[2] = "New York";
+    grupo[3] = "Londres";
+    grupo[4] = "Tegucigalpa";
+    grupo[5] = "San Pedro Sula";
+    grupo[6] = "Ciudad de Guatemala";
+    grupo[7] = "Bruselas";
+    grupo[8] = "Medellín";
+    grupo[9] = "Yonaguni";
+    grupo[10] = "La Ceiba";
+    grupo[11] = "Tela";
+    grupo[12] = "Ámsterdam";
+    grupo[13] = "Managua";
+    grupo[14] = "Roma";
+    grupo[15] = "Marcala";
+    grupo[16] = "Roatán";
+    grupo[17] = "Barcelona";
+    grupo[18] = "Madrid";
+    grupo[19] = "Valencia";
+    
+    System.out.println("El tamaño del arreglo es " +grupo.length);
+    System.out.println("1. " + grupo[0]);
+    System.out.println("2. " + grupo[1]);
+    System.out.println("3. " + grupo[2]);
+    System.out.println("4. " + grupo[3]);
+    System.out.println("5. " + grupo[4]);
+    System.out.println("6. " + grupo[5]);
+    System.out.println("7. " + grupo[6]);
+    System.out.println("8. " + grupo[7]);
+    System.out.println("9. " + grupo[8]);
+    System.out.println("10. " + grupo[9]);
+    System.out.println("11. " + grupo[10]);
+    System.out.println("12. " + grupo[11]);
+    System.out.println("13. " + grupo[12]);
+    System.out.println("14. " + grupo[13]);
+    System.out.println("15. " + grupo[14]);
+    System.out.println("16. " + grupo[15]);
+    System.out.println("17. " + grupo[16]);
+    System.out.println("18. " + grupo[17]);
+    System.out.println("19. " + grupo[18]);
+    System.out.println("20. " + grupo[19]);
+  }
+    
+ }
